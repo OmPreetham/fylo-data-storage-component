3df4c460e119cfdf53c431fe18e3487eb55168e9
Om Preetham <ompreethambandi@gmail.com>
2024-04-02 16:02:30 -0500

    setup vite boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import FyloDataStorage from './components/FyloDataStorage'
+
+const App = () => {
+  return <FyloDataStorage />
+}
+export default App
---
+++ src/components/FyloDataStorage.jsx
@@ -0,0 +1,8 @@
+const FyloDataStorage = () => {
+  return (
+    <div className="wrapper">
+      <main></main>
+    </div>
+  )
+}
+export default FyloDataStorage
